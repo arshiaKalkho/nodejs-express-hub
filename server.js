fdbcbbc75598880efefe1eeb5472631253fcb556
Arshia Kalkhorani <arshiabdol@yahoo.com>
2021-10-15 01:32:10 -0700

    fixed login page logic not working with new user

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
-  }
-  
+}
+
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt')
@@ -9,6 +9,7 @@ const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
+
 const initializePassport = require('./passport-config')
 
 
@@ -19,15 +20,18 @@ initializePassport(
     id => users.find(user => user.id === id)
 )
 
+const users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
 
 
-const users=[];//for now, this will be changed
 
 
 
-app.set('view-engine','ejs'); 
-app.use(express.urlencoded({extended: false}))
+
+
 app.use(flash())
 app.use(session({
     secret: process.env.SESSION_SECRET,
@@ -40,62 +44,61 @@ app.use(methodOverride('_method'))
 
 
 
-app.get('/', checkAuthenticated,(req,res)=>{
-    res.render('index.ejs',{name: req.user.name})
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
 })
 
-app.get('/login' ,checkNotAuthenticated, (req,res)=>{
-
+app.get('/login', checkNotAuthenticated, (req, res) => {
     res.render('login.ejs')
-    
 })
-app.get('/register', checkNotAuthenticated, (req,res)=>{
 
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
     res.render('register.ejs')
-
 })
-app.post('/register',checkNotAuthenticated ,async (req,res)=>{
 
-    res.render('register.ejs')
-
-    try{
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);//this will be saved in a data base
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
         users.push({
-            id:Data.now().toString(),
-            name:     req.body.name,
-            email:    req.body.email,
-            password: hashedPassword
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
 
 
-        })
+    })
         res.redirect('/login')
-    }catch{
+    } catch {
         res.redirect('/register')
-        
     }
 
-   
 })
 
-
-
-app.delete('/logout', (req, res) => {//loging out, forms don't support this by default
+app.delete('/logout', (req, res) => {
     req.logOut()
     res.redirect('/login')
-  }) 
 
-function checkAuthenticated(req, res, next) {//this middleware will always check to see if user is authenticated
+})
+
+function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next()
     }
 
     res.redirect('/login')
 }
-function checkNotAuthenticated(req, res, next) {//same, but this will activly redirect user to home
+
+function checkNotAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
-        return res.redirect('/')
+    return res.redirect('/')
     }
     next()
-  }
-  
+}
+
 app.listen(3000)

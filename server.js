0d75431b5126d11c86408cb5f7649e65e5c4d60e
Arshia Kalkhorani <arshiabdol@yahoo.com>
2021-10-18 23:43:44 -0700

    added d3 to create graph

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
-
 const initializePassport = require('./passport-config')
 
 
@@ -25,12 +24,6 @@ const users = []
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(__dirname));
-
-
-
-
-
-
 
 app.use(flash())
 app.use(session({

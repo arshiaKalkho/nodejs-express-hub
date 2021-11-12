5b9e10468a306143e77e04f0000657712f775f26
Arshia Kalkhorani <arshiabdol@yahoo.com>
2021-11-11 16:38:30 -0800

    working script with color change for the index file

--- views/darkmode.js
+++ views/darkmode.js
@@ -1,5 +1,7 @@
 let darkmode = localStorage.getItem("darkMode");
 const darkModeToggle = document.querySelector("#dark-mode-toggle");
+let vars = document.querySelector("html");
+
 
 
 
@@ -7,15 +9,23 @@ const darkModeToggle = document.querySelector("#dark-mode-toggle");
 
 const enableDarkMode = () =>{
     document.body.classList.add("darkmode");
+    vars.style.setProperty('--color-primary', "#000ffc" )
+    vars.style.setProperty('--color-secondary', "001220" )
+    vars.style.setProperty('--color-tertioary', "#00ddfc" )
 
     localStorage.setItem("darkMode", "enabled")
 };
 
 const disableDarkMode = () =>{
-    document.body.classList.remove("darkmode");
+    vars.style.setProperty('--color-primary', "#c62368" )
+    vars.style.setProperty('--color-secondary', "#001220" )
+    vars.style.setProperty('--color-tertioary', "#fa7268" )
 
     localStorage.setItem("darkMode", null)
 };
+
+
+//on page load check for local storage var darkmode's value
 if(darkmode === "enabled"){
     enableDarkMode()
 }
